8825f97ac009707dc1c57fd0095daa6108eeef6b
Tania Tatula <tgrogol@gmail.com>
2021-02-28 01:38:58 +0200

    трохи удостконалює 8 завдання

--- js/task-5.js
+++ js/task-5.js
@@ -2,18 +2,24 @@ const nameUserEl = document.querySelector('#name-input');
 const nameEl = document.querySelector('#name-output');
 
 
-console.log(nameEl.textContent);
 
-const handleSubmit = (event) => {
-    event.preventDefault();
+
+// const handleSubmit = (event) => {
+//     event.preventDefault();
 
     
-        if(event.currentTarget.value === ''){
-            return nameEl.textContent = 'незнакомец';
-        } 
-        return nameEl.textContent = event.currentTarget.value;
+//         if(event.currentTarget.value === ''){
+//             return nameEl.textContent = 'незнакомец';
+//         } 
+//         return nameEl.textContent = event.currentTarget.value;
         
     
-    }
+//     }
+
     nameUserEl.addEventListener('input', handleSubmit);
-   
+ 
+ 
+//  ---вирішення 2-----
+    function handleSubmit(event){
+    nameEl.textContent = event.currentTarget.value;
+   }
--- js/task-8.js
+++ js/task-8.js
@@ -5,6 +5,16 @@ const boxesEl = document.querySelector('#boxes');
 const numberEl = btnBox.firstElementChild;
 const boxStyles = boxesEl.children;
 
+// function getRandomInt(min, max) {
+//         return Math.floor(Math.random() * (max - min)) + min;
+//       }
+// const colorNumvber = 'rgb(' +`${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` +')' ;
+// const counter = 2;
+// const blockSize = 30 + counter * 10;
+// const b = `height: ${blockSize}px; width: ${blockSize}px; background-color: ${colorNumvber}`;
+// console.log(b)
+// boxesEl.insertAdjacentHTML('afterbegin', `<div style='height: '${blockSize}px'; width: '${blockSize}px'; background-color: '${colorNumvber}'></div>`);
+
 
 const handleClearOutput = () => {
     
@@ -14,29 +24,34 @@ const handleClearOutput = () => {
 
 const handleIntroductionNumber = () => {
 
-    // ---зробити, щоб додавало без циклу за один прихід-----
+    // --- не виходить зробити, щоб додавало без циклу за один раз і додати стилі-----
   const numberOfInputs = numberEl.value
   let counter = 1;
     while(counter <= numberOfInputs){
-    boxesEl.insertAdjacentHTML('afterbegin', '<div class="list-styles"></div>');
+     
+        
+    boxesEl.insertAdjacentHTML('afterbegin', '<div></div>');
+  
     counter += 1;
     }
+
+    while(counter <= numberOfInputs){
+     
+        
+      function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+      const colorNumvber = 'rgb(' +`${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` +')' ;
+      const blockSize = 30 + counter * 10;
+      boxStyles.setAttribute("style", `height: ${blockSize}px; width: ${blockSize}px; background-color: ${colorNumvber}`);
+      counter += 1;
+      }
+    
+    
     
 }
 
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-const colorNumvber = 'rgb(' +`${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` + ',' + `${getRandomInt(0, 1000)}` +')' ;
-console.log(colorNumvber);
-
-
-boxStyles.style.backgroundColor = `${colorNumvber}`;
-boxStyles.style.backgroundColor = `${colorNumvber}`;
-boxStyles.style.width = '30px';
-boxStyles.style.heigt = '30px';
 
 btnDestroy.addEventListener('click', handleClearOutput);
 btnRender.addEventListener('click', handleIntroductionNumber);
@@ -45,5 +60,3 @@ btnRender.addEventListener('click', handleIntroductionNumber);
 
 
 
-  
- 
