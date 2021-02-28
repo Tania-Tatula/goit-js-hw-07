3880957389f55b5435c29adc73fc0a4d97182b84
Tania Tatula <tgrogol@gmail.com>
2021-02-28 12:24:56 +0200

    правильно вирішує завдання 8

--- js/task-8.js
+++ js/task-8.js
@@ -1,3 +1,4 @@
+// ------вирішення 1------------
 // const btnBox = document.querySelector("#controls");
 // const btnDestroy = btnBox.lastElementChild;
 // const btnRender = btnDestroy.previousElementSibling;
@@ -43,6 +44,51 @@
 // btnDestroy.addEventListener("click", handleClearOutput);
 // btnRender.addEventListener("click", handleIntroductionNumber);
 
+
+// ------вирішення 2------------
+
+// const btnBox = document.querySelector("#controls");
+// const btnDestroy = btnBox.lastElementChild;
+// const btnRender = btnDestroy.previousElementSibling;
+// const boxesEl = document.querySelector("#boxes");
+// const numberEl = btnBox.firstElementChild;
+
+// const handleClearOutput = () => {
+//   boxesEl.innerHTML = "";
+// };
+
+// const handleIntroductionNumber = () => {
+//   // --- не виходить зробити, щоб додавало без циклу за один раз-----
+//   const numberOfInputs = numberEl.value;
+//   let counter = 1;
+//   while (counter <= numberOfInputs) {
+//     function getRandomInt(min, max) {
+//       return Math.floor(Math.random() * (max - min)) + min;
+//     }
+//     const colorNumvber =
+//       "rgb(" +
+//       `${getRandomInt(0, 1000)}` +
+//       "," +
+//       `${getRandomInt(0, 1000)}` +
+//       "," +
+//       `${getRandomInt(0, 1000)}` +
+//       ")";
+//     const blockSize = 30 + counter * 10;
+
+//     const boxingElementCreator = boxesEl.insertAdjacentHTML(
+//       "afterbegin",
+//       `<div style='height: ${blockSize}px; width: ${blockSize}px; background-color: ${colorNumvber}'></div>`
+//     );
+
+//     counter += 1;
+//   }
+// };
+
+// btnDestroy.addEventListener("click", handleClearOutput);
+// btnRender.addEventListener("click", handleIntroductionNumber);
+
+
+
 const btnBox = document.querySelector("#controls");
 const btnDestroy = btnBox.lastElementChild;
 const btnRender = btnDestroy.previousElementSibling;
@@ -54,10 +100,13 @@ const handleClearOutput = () => {
 };
 
 const handleIntroductionNumber = () => {
-  // --- не виходить зробити, щоб додавало без циклу за один раз-----
+
   const numberOfInputs = numberEl.value;
-  let counter = 1;
-  while (counter <= numberOfInputs) {
+  const result = [];
+  
+  for (let i = 0; i < numberOfInputs; i += 1) {
+    const divs = document.createElement("div");
+    console.log(divs);
     function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
     }
@@ -69,18 +118,13 @@ const handleIntroductionNumber = () => {
       "," +
       `${getRandomInt(0, 1000)}` +
       ")";
-    const blockSize = 30 + counter * 10;
+    const blockSize = 30 + i * 10;
+    divs.style.cssText = `height: ${blockSize}px; width: ${blockSize}px; background-color: ${colorNumvber}`;
 
-    const boxingElementCreator = boxesEl.insertAdjacentHTML(
-      "afterbegin",
-      `<div style='height: ${blockSize}px; width: ${blockSize}px; background-color: ${colorNumvber}'></div>`
-    );
-
-    counter += 1;
+    result.push(divs);
   }
+  boxesEl.append(...result);
 };
 
 btnDestroy.addEventListener("click", handleClearOutput);
 btnRender.addEventListener("click", handleIntroductionNumber);
-
-
